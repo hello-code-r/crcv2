ae32cf124c9544d18394e83c3874d3384d268dca
hello-code-r <shiva.chilukuri@outlook.com>
2021-01-10 10:53:33 -0800

    hello

--- js/Game.js
+++ js/Game.js
@@ -34,6 +34,11 @@ class Game {
     car3 = createSprite(500,200);
     car4 = createSprite(700,200);
     cars = [car1, car2, car3, car4];
+    
+    car1.addImage(car1Image);
+    car2.addImage(car2Image);
+    car3.addImage(car3Image);
+    car4.addImage(car4Image);
   }
 
   play(){
@@ -42,8 +47,9 @@ class Game {
     Player.getPlayerInfo();
     
     if(allPlayers !== undefined){
+      background("red"); 
       //var display_position = 100;
-      
+      image(track,0, -displayHeight*4,displayWidth, displayHeight*5);
       //index of the array
       var index = 0;
 
@@ -56,7 +62,7 @@ class Game {
         index = index + 1 ;
 
         //position the cars a little away from each other in x direction
-        x = x + 200;
+        x = x + 400;
         //use data form the database to display the cars in y direction
         y = displayHeight - allPlayers[plr].distance;
         cars[index-1].x = x;
@@ -79,6 +85,13 @@ class Game {
       player.update();
     }
 
+    if(player.distance>500){
+      gameState=2;
+    }
+
     drawSprites();
+  }
+  end(){
+  console.log("GAME OVER!!");
   }
 }
